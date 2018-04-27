10c28dc02838111d71b7ac89b6fb50242162652f
Unknown <ryan2002.rs@gmail.com>
2018-04-27 21:07:22 +1000

    AI snake started

--- snake-js/src/sketch.js
+++ snake-js/src/sketch.js
@@ -1,24 +1,134 @@
-let snake;
-let food;
+let snakes=[];
+let deadSnakes=[];
+const SNAKE_COUNT=100;
+let neat;
+let slider;
+let speed=1;
+let count=0;
+let highestScore=0;
+let generation=1;
+
+let highScoreText;
+let scoreText;
+let loadBestButton;
+
+let chartFitnesses=[];
+let chartLabels=[];
+
+
 function setup() {
-    createCanvas(500, 500);
-    snake=new Snake();
-    newFood();
+    let canvas=createCanvas(500, 500);
+    canvas.style('display', 'block');
+    canvas.parent('sketch-holder');
+
+    neat=new neataptic.Neat(12,4,null,
+        {
+            mutation: neataptic.methods.mutation.ALL,
+            popsize: SNAKE_COUNT,
+            mutationRate: 0.3,
+            elitism: Math.round(0.1* SNAKE_COUNT),
+            network: new neataptic.architect.Random(
+                12,
+                6,
+                4
+            )
+        });
+
+
+    newGeneration();
+    //ui stuff
+    slider = createSlider(1, 2000, 1);
+
+    highScoreText = createElement("p", "");
+    highScoreText.position(0, height + 10);
+    scoreText = createElement("p", "");
+    scoreText.position(0, height + 30);
+    loadBestButton = createButton("Load pre-trained bird");
+    loadBestButton.position(0, height + 65);
+    loadBestButton.mousePressed(() => {
+        this.snake = [];
+        this.snake.push(new Snake());
+
+        //snakes[0].brain = generateTrainedBrain();
+    })
+}
+
+function updateChart(score) {
+
+}
+
+function newGeneration(){
+    if(deadSnakes.length===0){
+        for (let i = 0; i < SNAKE_COUNT; i++) {
+            snakes[i]=new Snake();
+        }
+        return;
+    }
+    //calculate fitness
+    let sum=0;
+    for(let snake of deadSnakes){
+        sum+=snake.score;
+    }
+    for(let snake of deadSnakes){
+        snake.fitness=snake.score/sum;
+    }
+
+    let bestSnake;
+    let index=0;
+    let r=random(1);
+    while(r>0){
+        r=r-deadSnakes[index].fitness;
+        index++;
+    }
+
+    bestSnake=deadSnakes[index-1];
+    for (let i = 0; i < SNAKE_COUNT; i++) {
+        snakes[i]=new Snake();
+        /*if(i<SNAKE_COUNT-50)//we want some birds to be completely random so the network can break out of stand stills where no one progresses
+            snakes[i].setBrain(bestSnake.brain);//sets brain with mutations*/
+    }
+    generation++;
+
+    updateChart(bestSnake.score);
+    deadSnakes=[];
+    count=0;
+    console.log('new generation');
+
+
 }
 
 function update(){
-snake.update();
-food.update();
-let head=snake.getHead();
-if(snake.hitItself()||head.x>width-20||head.y>height-20||head.x<20||head.y<20){//if snake died
-    snake=new Snake();//make new snake and food
-    newFood();
-}
-if(food.hasBeenEaten(snake)){
-    snake.score++;
-    snake.addLength();
-    newFood();
-}
+    if (frameCount % 10 === 0)
+        highScoreText.html("Highest Score: " + highestScore);
+    if(snakes.length===0){
+        newGeneration();
+        return;
+    }
+    if (frameCount % 10 === 0) {
+        scoreText.html("Current Score: " + snakes[0].score);
+    }
+    if (frameCount % 60 === 0)
+        updateChart(snakes[0].score);
+    if (speed <= 2000)
+        speed=slider.value();
+    for(let i=snakes.length-1;i>=0;i--){
+        let snake=snakes[i];
+        let head=snake.getHead();
+        if(snake.hitItself()||head.x>width-20||head.y>height-20||head.x<20||head.y<20||snake.moves<0){//if snake died
+            deadSnakes.push(snakes.splice(i,1)[0]);
+            continue;
+        }
+        snake.think();
+        snake.update();
+        if (snake.score > highestScore)
+            highestScore = snake.score;
+
+    }
+
+
+
+
+
 
 
 }
@@ -26,9 +136,12 @@ if(food.hasBeenEaten(snake)){
 
 function draw() {
     background(0);
-    update();
-    snake.render();
-    food.render();
+    for (let i = 0; i <speed ; i++) {
+        update();
+    }
+    snakes.forEach((snake)=>{
+        snake.render();
+    })
 
     drawBox();
 
@@ -45,7 +158,7 @@ function drawBox() {
 function newFood(){
     let xFruit = floor(random(10, (width - 100) / 10)) * 10;
     let yFruit = floor(random(10, (height - 100) / 10)) * 10;
-    food=new Food(xFruit,yFruit);
+    return new Food(xFruit,yFruit);
 }
 function Food(x,y){
     this.x=x;
@@ -53,9 +166,9 @@ function Food(x,y){
     this.update=()=>{
 
     };
-    this.render=()=>{
+    this.render=(color)=>{
       fill(255,0,0);
-      stroke(255,0,0);
+      stroke(color[0],color[1],color[2]);
       strokeWeight(10);
       point(this.x,this.y);
 
--- snake-js/src/snake.js
+++ snake-js/src/snake.js
@@ -8,6 +8,10 @@ class Snake{
         this.y=[];
         this.numSegments=4;//start off with 4 length
         this.score=0;
+        this.fitness=0;
+        this.moves=500;
+        this.food=newFood();
+        this.color=[random(255),random(255),random(255)];
         frameRate(15);
         this.direction=RIGHT_ARROW;
         for (let i = 0; i < this.numSegments; i++) {
@@ -15,9 +19,19 @@ class Snake{
             this.y.push(250);//250 is where snake starts on y
         }
 
-    }
-    update() {
+        //neural network
+        this.brain = neataptic.architect.Perceptron(12, 6, 4);
 
+    }
+    setBrain(brain){
+
+        this.brain=neataptic.Network.fromJSON(brain.toJSON());
+        this.brain.mutate(neataptic.methods.mutation.MOD_BIAS);
+        this.brain.mutate(neataptic.methods.mutation.MOD_WEIGHT);
+    }
+
+    update() {
+        this.moves--;
         for (let i = 0; i < this.numSegments - 1; i++) {
             this.x[i] = this.x[i + 1];
             this.y[i] = this.y[i + 1];
@@ -40,6 +54,13 @@ class Snake{
                 this.y[this.numSegments - 1] = this.y[this.numSegments - 2] + DIFF;
                 break;
         }
+        this.food.update();
+        if(this.food.hasBeenEaten(this)){
+            this.score++;
+            this.addLength();
+            this.food=newFood();
+            this.moves=500;
+        }
 
 
     }
@@ -50,14 +71,60 @@ class Snake{
     render(){
         push();
         fill(255);
-        stroke(255);
+        stroke(this.color[0],this.color[1],this.color[2],100);
         strokeWeight(10);
         for (let i = 0; i < this.numSegments - 1; i++) {
+            if(i===this.numSegments-2)//TODO remove
+                stroke(255,0,0);
             line(this.x[i], this.y[i], this.x[i + 1], this.y[i + 1]);
         }
         pop();
+        this.food.render(this.color);
 
     }
+    think(){
+        let inputs=[];
+        //right 0,1,2 are food, wall, itself
+        //left 3,4,5 are food, wall, itself
+        //up 6,7,8 are food, wall, itself
+        //down 9,10,11 are food, wall, itself
+        for (let i = 0; i < 12; i++) {
+            inputs[i]=1;
+
+        }
+        let results=[rayCast(this.getHead().x,this.getHead().y,RIGHT_ARROW,this),
+        rayCast(this.getHead().x,this.getHead().y,LEFT_ARROW,this),
+        rayCast(this.getHead().x,this.getHead().y,UP_ARROW,this),
+        rayCast(this.getHead().x,this.getHead().y,DOWN_ARROW,this)];
+        for (let i = 0; i <=9; i+=3) {
+            if(results[i/3].result==='food')
+                inputs[i]=map(results[i/3].distance,20,Math.max(width,height),0,1);
+
+        }
+        for (let i = 1; i <=10 ; i+=3) {
+            if(results[(i-1)/3].result==='wall')
+                inputs[i]=map(results[(i-1)/3].distance,10,Math.max(width,height),0,1)
+
+        }
+        for (let i = 2; i <=11 ; i+=3) {
+            if(results[(i-2)/3].result==='itself')
+                inputs[i]=map(results[(i-2)/3].distance,10,Math.max(width,height),0,1)
+
+        }
+       // console.log(inputs);
+
+        let activated=this.brain.activate(inputs);
+
+        if (activated[0] > 0.5)
+            this.direction=UP_ARROW;
+        if (activated[1] > 0.5)
+            this.direction=DOWN_ARROW;
+        if (activated[2] > 0.5)
+            this.direction=LEFT_ARROW;
+        if (activated[3] > 0.5)
+            this.direction=RIGHT_ARROW;
+    }
+
     hitItself(){
         let head=this.getHead();
         for (let i = 0; i < this.x.length-1; i++) {//ignore last part as thats head.
@@ -79,18 +146,18 @@ class Snake{
 
 
 }
-function keyPressed(){
-if (keyCode === LEFT_ARROW&&snake.direction!==RIGHT_ARROW) {
-    snake.direction=LEFT_ARROW;
+/*function keyPressed(){
+if (keyCode === LEFT_ARROW&&snakes[0].direction!==RIGHT_ARROW) {
+    snakes[0].direction=LEFT_ARROW;
 
-} else if (keyCode === RIGHT_ARROW&&snake.direction!==LEFT_ARROW) {
-    snake.direction=RIGHT_ARROW;
+} else if (keyCode === RIGHT_ARROW&&snakes[0].direction!==LEFT_ARROW) {
+    snakes[0].direction=RIGHT_ARROW;
 }
-else if (keyCode === UP_ARROW&&snake.direction!==DOWN_ARROW) {
-    snake.direction=UP_ARROW;
+else if (keyCode === UP_ARROW&&snakes[0].direction!==DOWN_ARROW) {
+    snakes[0].direction=UP_ARROW;
 }
-else if (keyCode === DOWN_ARROW&&snake.direction!==UP_ARROW) {
-    snake.direction=DOWN_ARROW;
+else if (keyCode === DOWN_ARROW&&snakes[0].direction!==UP_ARROW) {
+    snakes[0].direction=DOWN_ARROW;
 }
 
 
@@ -98,18 +165,76 @@ else if (keyCode === DOWN_ARROW&&snake.direction!==UP_ARROW) {
 }
 function touchMoved(){
     if(mouseX>width/2){
-        if(mouseY<height/2&&snake.direction!==DOWN_ARROW)
-            snake.direction=UP_ARROW;
-        else if(snake.direction!==LEFT_ARROW) snake.direction=RIGHT_ARROW;
+        if(mouseY<height/2&&snakes[0].direction!==DOWN_ARROW)
+            snakes[0].direction=UP_ARROW;
+        else if(snakes[0].direction!==LEFT_ARROW) snakes[0].direction=RIGHT_ARROW;
     }
     else{
-        if(mouseY<height/2&&snake.direction!==RIGHT_ARROW)
-            snake.direction=LEFT_ARROW;
-        else if(snake.direction!==UP_ARROW) snake.direction=DOWN_ARROW;
+        if(mouseY<height/2&&snakes[0].direction!==RIGHT_ARROW)
+            snakes[0].direction=LEFT_ARROW;
+        else if(snakes[0].direction!==UP_ARROW) snakes[0].direction=DOWN_ARROW;
     }
 
 }
 function touchStarted(){
     touchMoved();
+}*/
+function rayCast(x,y,direction,snake){
+    let currentPos=createVector(x,y);
+    let tries=0;
+    let findItem=(snake)=>{
+        if(currentPos.x>=width-10||currentPos.y>=height-10||currentPos.x<=-10||currentPos.y<=-10)
+            return 'wall';
+        if(currentPos.x===snake.food.x&&currentPos.y===snake.food.y)
+            return 'food';
+
+        for (let i = 0; i < snake.numSegments-1; i++) {
+
+                let segPos=createVector(snake.x[i],snake.y[i]);
+                if(segPos.equals(currentPos)&&tries!==0){
+                    return 'itself';
+
+
+            }
+        }
+        return -1;
+    };
+    let result=-1;
+    switch(direction){
+        case UP_ARROW:
+
+            while(result===-1&&tries<5000){
+                result = findItem(snake);
+                currentPos.add(0,-DIFF);
+                tries++;
+            }
+            return {result: result, distance: y-currentPos.y};
+        case DOWN_ARROW:
+            while(result===-1&&tries<5000){
+                result = findItem(snake);
+                currentPos.add(0,DIFF);
+                tries++;
+            }
+            return {result: result, distance: currentPos.y-y};
+        case LEFT_ARROW:
+            while(result===-1&&tries<5000){
+                result = findItem(snake);
+                currentPos.add(-DIFF,0);
+                tries++;
+            }
+            return {result: result, distance: x-currentPos.x};
+        case RIGHT_ARROW:
+            while(result===-1&&tries<5000){
+                result = findItem(snake);
+                currentPos.add(DIFF,0);
+                tries++;
+            }
+
+            return {result: result, distance: currentPos.x-x};
+
+
+
+    }
+
 }
 
