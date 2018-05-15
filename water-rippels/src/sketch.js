35ae0049dd4a8b6c22693013e09a35e891502a18
SimplyBallistic <ryan2002.rs@gmail.com>
2018-05-15 18:30:02 +1000

    Better water Ripples

--- water-rippels/src/sketch.js
+++ water-rippels/src/sketch.js
@@ -1,10 +1,11 @@
 let dampening = 0.95;
 let previous=[], current=[],cols,rows;
+let slider;
 
 function setup() {
     // put setup code here
     let canvas = createCanvas(500, 500);
-
+    slider=createSlider(0.87,0.9999,0.95,0.001);
     pixelDensity(1);
     cols = width;
     rows = height;
@@ -25,6 +26,7 @@ function update() {
     if(frameCount%5===0){
         current[int(random(0,cols))][int(random(0,rows))]=255;
     }
+    dampening=slider.value();
 
 
 }
@@ -43,9 +45,9 @@ function draw() {
                 ) / 4 - current[i][j];
             current[i][j] = current[i][j] * dampening;
             let index = (i + j * cols) * 4;
-            pixels[index] = current[i][j] * 50;
-            pixels[index + 1] = current[i][j] * 50;
-            pixels[index + 2] = current[i][j] * 145+115;
+            pixels[index] = current[i][j] * 5;
+            pixels[index + 1] = current[i][j] * 5;
+            pixels[index + 2] = current[i][j] * 10+100;
             pixels[index + 3] = 255
         }
     }
